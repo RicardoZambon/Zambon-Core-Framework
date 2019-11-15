36aa02a0c151a9ea150ab23adfab5b20d94ca56b
Ricardo Zambon <ricardo.zambon@smartm.com>
2019-11-15 17:00:47 -0300

    Adjusting XML references path.
    Updated font awesome version in package.json.
    
    Expanded the menu in 20px.

--- Zambon.Core.WebModule/gulpfile.js
+++ Zambon.Core.WebModule/gulpfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding Clean='clean, minify, scripts' />
+﻿/// <binding BeforeBuild='scripts' Clean='clean, minify, scripts' />
 /*
 This file in the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
